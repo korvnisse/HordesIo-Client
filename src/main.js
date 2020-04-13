b3aaba9a3b2b4ba552ba41c89d596aadee2a9d28
korvnisse <SSandstrom95@gmail.com>
2020-04-13 23:28:37 +0200

    0.5.1 just testing update.

--- src/main.js
+++ src/main.js
@@ -40,10 +40,6 @@ function handleSquirrelEvent() {
     switch (squirrelEvent) {
         case '--squirrel-install':
         case '--squirrel-updated':
-            // Optionally do things such as:
-            // - Add your .exe to the PATH
-            // - Write to the registry for things like file associations and
-            //   explorer context menus
 
             // Install desktop and start menu shortcuts
             spawnUpdate(['--createShortcut', exeName]);
@@ -52,8 +48,6 @@ function handleSquirrelEvent() {
             return true;
 
         case '--squirrel-uninstall':
-            // Undo anything you did in the --squirrel-install and
-            // --squirrel-updated handlers
 
             // Remove desktop and start menu shortcuts
             spawnUpdate(['--removeShortcut', exeName]);
@@ -63,9 +57,9 @@ function handleSquirrelEvent() {
 
         case '--squirrel-obsolete':
             // This is called on the outgoing version of your app before
-            // we update to the new version - it's the opposite of
-            // --squirrel-updated
+            // we update to the new version -
 
+            //Copy cookies here?
             app.quit();
             return true;
     }
