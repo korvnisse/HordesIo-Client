5d5478845136f6e79001ff726c69058435841901
korvnisse <SSandstrom95@gmail.com>
2020-04-13 17:57:15 +0200

    Just testing update Func

--- src/main.js
+++ src/main.js
@@ -99,10 +99,10 @@ function createWindow() {
 
     //destroy win on close
     win.on('closed', () => {
-            view = null
-            win = null
-        })
-        //view.webContents.openDevTools() //-->Devtools for debugging<--
+        view = null
+        win = null
+    })
+    view.webContents.openDevTools() //-->Devtools for debugging<--
 }
 
 app.on('ready', createWindow)
